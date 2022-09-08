0d8b2a9efd86ceb9f6f77bde9d3ed78d28934ae0
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-09-08 16:13:45 +0200

    Update main.py
    simple setup

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-# using flask_restful
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
-
-# creating the flask app
-app = Flask(__name__)
-# creating an API object
-api = Api(app)
-
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
-# other methods include put, delete, etc.
-class Hello(Resource):
-
-	# corresponds to the GET request.
-	# this function is called whenever there
-	# is a GET request for this resource
-	def get(self):
-
-		return jsonify({'message': 'hello world'})
-
-	# Corresponds to POST request
-	def post(self):
-		
-		data = request.get_json()	 # status code
-		return jsonify({'data': data}), 201
+from flask import Flask, request, jsonify
 
 
-# another resource to calculate the square of a number
-class Square(Resource):
 
-	def get(self, num):
-
-		return jsonify({'square': num**2})
+app = Flask(__name__) # 1
 
 
-# adding the defined resources along with their corresponding urls
-api.add_resource(Hello, '/')
-api.add_resource(Square, '/square/<int:num>')
+
+@app.route('/') # 2
+
+def hello():
+
+    number = request.args.get("number")
+
+    if number == '+3155512345':
+
+        return jsonify({'exists': True})
+
+    else:
+
+        return jsonify({'exists': False}) # 3
 
 
-# driver function
-if __name__ == '__main__':
 
-	app.run(debug = True)
+app.run(port=8080)
