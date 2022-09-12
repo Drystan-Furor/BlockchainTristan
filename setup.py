2d7db0b6789a9c7eba26a7ae4f67a604ace0de22
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-09-12 09:14:37 +0200

    boilerplate
    om de learning curve iets te verkleinen hierbij wat boilerplate code om te kunnen beginnen met de opdrachten van bijeenkomst 1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='blockchain',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'pytest',
+        'pytest-cov'
+    ],
+)
