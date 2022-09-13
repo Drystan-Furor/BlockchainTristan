773cbffabcc5fc10e2928d047b486d2fb2c0a7fc
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-09-13 15:01:27 +0200

    Update blockchain.py

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,4 +1,19 @@
-class Blockchain(object):
+class Blockchain(object): 
+    """
+    block = {
+    'index': 1,
+    'timestamp': 1506057125.900785,
+    'transactions': [
+        {
+            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+            'amount': 5,
+        }
+    ],
+    'proof': 324984774000,
+    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    }
+    """
     def __init__(self):
         self.chain = []
         self.current_transactions = []
@@ -7,9 +22,22 @@ class Blockchain(object):
         # Creates a new Block and adds it to the chain
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
