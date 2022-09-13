b8710466e881c862c48fbaf7969827ca0fb6c09c
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-09-13 20:45:57 +0200

    things 2

--- blockchain/api.py
+++ blockchain/api.py
@@ -1,69 +1,34 @@
+import functools
 
-from textwrap import dedent
-from uuid import uuid4
-from flask import Flask, jsonify, request
-from blockchain import Blockchain
-# Instantiate our Node
-app = Flask(__name__)
+from flask import (
+    Blueprint, jsonify
+)
 
-# Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
+bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
-# Instantiate the Blockchain
-blockchain = Blockchain()
-
-@app.route('/mine', methods=['GET'])
-def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
-
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
+@bp.route('/blocks', methods=['GET'])
+def get_blocks():
+    response = {'test': 'test'}
     return jsonify(response), 200
 
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    values = request.get_json()
+@bp.route('/block/add', methods=['POST'])
+def add_block():
+    pass
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
+@bp.route('/transaction/create', methods=['POST'])
+def create_transaction():
+    pass
 
-    # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
-
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
+@bp.route('/transactions', methods=['GET'])
+def get_transactions():
+    response = {'test': 'test'}
     return jsonify(response), 200
 
+@bp.route('/node/create', methods=['POST'])
+def create_node():
+    pass
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@bp.route('/node/register', methods=['POST'])
+def register_node():
+    pass
+
--- blockchain/block.py
+++ blockchain/block.py
@@ -1,6 +1,14 @@
-import hashlib
+from hashlib import sha256
 import json
 import time
 
-
-class Block():
+class Block:
+    def __init__(self, index, timestamp, previous_hash, nonce=0) -> None:
+        self.index = index
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+    
+    def hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
