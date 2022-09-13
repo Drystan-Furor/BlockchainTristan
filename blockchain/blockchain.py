fe1d0bc0f283ccd0b7217412c6396cd1489d6eb9
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-09-13 14:58:36 +0200

    fucking up

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 class Blockchain:
     def __init__(self) -> None:
         pass
+    
+    def create_genesis_block():
+            block = Block(index=0,
+                  timestamp=datetime.now(),
+                  data="Genesis Block",
+                  previous_hash="0")
+    return block
